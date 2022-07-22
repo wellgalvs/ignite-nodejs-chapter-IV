46bcec748252288d6cda174a117e25e86b3c2fe6
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-07-22 09:48:28 -0300

    Carros - Alterando a Importação dos Repositórios

---
+++ chapter-IV-carros/src/modules/cars/infra/typeorm/repositories/SpecificationsRepository.ts
@@ -0,0 +1,32 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateSpecificationDTO, ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
+
+import { Specification } from "../entities/Specification";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private repository: Repository<Specification>
+
+  constructor() {
+    this.repository = getRepository(Specification);
+  }
+
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = await this.repository.create({
+      name,
+      description,
+    });
+
+    await this.repository.save(specification);
+  }
+
+  async findByname(name: string): Promise<Specification> {
+    const specification = await this.repository.findOne({
+      name,
+    });
+
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
---
+++ chapter-IV-carros/src/shared/container/index.ts
@@ -0,0 +1,23 @@
+import { container } from "tsyringe";
+
+import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
+import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+
+container.registerSingleton<ICategoriesRepository>(
+  "CategoriesRepository",
+  CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationsRepository>(
+  "SpecificationsRepository",
+  SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
+);
