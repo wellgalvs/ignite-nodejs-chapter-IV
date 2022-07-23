1dc2383d105f4b8386abc2fb6aa6b43752ef4845
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-07-23 12:56:34 -0300

    Carros - TDD na Prática

---
+++ chapter-IV-carros/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,24 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+let createCarUseCase: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
+
+describe("Create Car", () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+
+  it("Should be able to create a new car", async () => {
+    await createCarUseCase.execute({
+      name: "Name Car",
+      description: "Description Car",
+      daily_rate: 100,
+      license_plate: "ABC2A45",
+      fine_amount: 60,
+      brand: "Brand Car",
+      category_id: "Category"
+    });
+  })
+});
