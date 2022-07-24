d87e559a3fcdd0b91088bf38d772e68ae565223d
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-07-24 14:03:29 -0300

    Carros - Estruturando a Entidade de Carros

---
+++ chapter-IV-carros/src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -0,0 +1,49 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+
+import { Car } from "../entities/Car";
+
+class CarsRepository implements ICarsRepository {
+  private repository: Repository<Car>;
+
+  constructor() {
+    this.repository = getRepository(Car);
+  }
+
+  async create({
+    name,
+    description,
+    fine_amount,
+    license_plate,
+    daily_rate,
+    brand,
+    category_id
+  }: ICreateCarDTO): Promise<Car> {
+    const car = this.repository.create({
+      name,
+      description,
+      fine_amount,
+      license_plate,
+      daily_rate,
+      brand,
+      category_id
+    });
+
+    await this.repository.save(car);
+
+    return car;
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    const car = await this.repository.findOne({
+      license_plate
+    });
+
+    return car;
+  }
+
+}
+
+export { CarsRepository };
