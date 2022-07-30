dc15b57a66c7abd2c163c9a4a81254b4778558cf
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-07-30 19:07:01 -0300

    Aluguel - Trabalhando Datas com DayJs

--- chapter-IV-aluguel/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ chapter-IV-aluguel/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
@@ -11,10 +12,45 @@ describe("Create Rental", () => {
   });
 
   it("Should be able to create a new rental", async () => {
-    await createRentalUseCase.execute({
+    const rental = await createRentalUseCase.execute({
       user_id: "1234",
       car_id: "1111",
       expected_return_date: new Date()
     });
+
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
+  });
+
+  it("Should not be able to create a new rental if there is another open to the same car", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "1234",
+        car_id: "1111",
+        expected_return_date: new Date()
+      });
+
+      const rental = await createRentalUseCase.execute({
+        user_id: "1234",
+        car_id: "1111",
+        expected_return_date: new Date()
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should not be able to create a new rental if there is another open to the same user", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "1234",
+        car_id: "111",
+        expected_return_date: new Date()
+      });
+
+      const rental = await createRentalUseCase.execute({
+        user_id: "1234",
+        car_id: "1111",
+        expected_return_date: new Date()
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
