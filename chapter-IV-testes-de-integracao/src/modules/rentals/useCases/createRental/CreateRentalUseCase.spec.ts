0251993c3008fd89272075d06a63f41cc07c3967
Wellington G. Alves <wellingtonalvs@gmail.com>
2022-08-02 10:17:51 -0300

    Testes de Integração - Configurando Supertest

---
+++ chapter-IV-testes-de-integracao/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -0,0 +1,78 @@
+import dayjs from "dayjs";
+
+import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import { AppError } from "@shared/errors/AppError";
+
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
+
+
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let createRentalUseCase: CreateRentalUseCase;
+let dayJsDateProvider: DayjsDateProvider;
+
+describe("Create Rental", () => {
+  const dayAdd24Hours = dayjs().add(1, "day").toDate();
+
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    dayJsDateProvider = new DayjsDateProvider();
+    createRentalUseCase = new CreateRentalUseCase(
+      rentalsRepositoryInMemory,
+      dayJsDateProvider
+    );
+  });
+
+  it("Should be able to create a new rental", async () => {
+    const rental = await createRentalUseCase.execute({
+      user_id: "1234",
+      car_id: "1111",
+      expected_return_date: dayAdd24Hours
+    });
+
+    expect(rental).toHaveProperty("id");
+    expect(rental).toHaveProperty("start_date");
+  });
+
+  it("Should not be able to create a new rental if there is another open to the same car", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "1234",
+        car_id: "1111",
+        expected_return_date: dayAdd24Hours
+      });
+
+      const rental = await createRentalUseCase.execute({
+        user_id: "1234",
+        car_id: "1111",
+        expected_return_date: dayAdd24Hours
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should not be able to create a new rental if there is another open to the same user", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "1234",
+        car_id: "111",
+        expected_return_date: dayAdd24Hours
+      });
+
+      const rental = await createRentalUseCase.execute({
+        user_id: "1234",
+        car_id: "1111",
+        expected_return_date: dayAdd24Hours
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("Should not be able to create a new rental with invalid return time", async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: "1234",
+        car_id: "111",
+        expected_return_date: dayjs().toDate()
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
---
+++ chapter-IV-testes-de-integracao/src/shared/infra/typeorm/index.ts
@@ -0,0 +1,25 @@
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
+
+export default async (host = "database_ignite"): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host,
+    })
+  );
+};
+
+/*
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then((options) => {
+  const newOptions = options as IOptions;
+  newOptions.host = "database_ignite";
+  createConnection({
+    ...options,
+  });
+});
+*/
